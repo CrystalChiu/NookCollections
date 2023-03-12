307ab478515c161c17a90fd5f79fa53d1bded2f5
Crystal Chiu <chiubacca1011@gmail.com>
2023-03-12 05:00:05 -0700

    Optimized and debugged nookcollections.py. Printed array results in index.html

--- nookcollections.py
+++ nookcollections.py
@@ -1,4 +1,5 @@
 #from pickle import FALSE, TRUE
+from pickle import FALSE, TRUE
 from flask import Flask, render_template, request_started, url_for, jsonify, request, send_file
 import requests
 #from requests import get
@@ -11,54 +12,15 @@ app = Flask(__name__)
 
 # O(1) time, O(1) space
 def validTime(timeRange, curTime):
-    regex = r"(\d+)(\w\w)\s-\s(\d+)(\w\w)"
-    result = re.search(regex, timeRange)
-
-    # when None is returned, there is no time -> always available
-    if(result == None):
-        return True
-    else:
-        # store capture groups in int and string variables
-        startTime = int(result.group(1))
-        startMeridiem = str(result.group(2))
-        endTime = int(result.group(3))
-        endMeridiem = str(result.group(4))
-
-        # adjust to military time
-        start = startTime
-        if(startMeridiem == 'pm'):
-            start += 12
-        end = endTime
-        if(endMeridiem == 'pm'):
-            end += 12
-
-        # check if user's time is between start and end time
-        if(curTime >= start and curTime <= end):
-            return True
-        return False
+    if(curTime in timeRange): return True
+    return False
 
 # O(n) time, O(1) space
+#takes in the current month and the months the critter is available
+#outputs whether or not the current month falls within available range (true) or not (false)
 def validMonth(monthRange, curMonth):
-    regex = r"(\d+)-(\d+)"
-    result = re.search(regex, monthRange)
-    NUM_MONTHS = 12
-
-    if(result == None):
-        return True
-    else:
-        startMonth = int(result.group(1)) # start month (1-12)
-        endMonth = int(result.group(2)) # end month (1-12)
-
-        if(curMonth >= startMonth):
-            if(curMonth >= startMonth and curMonth <= 12):
-                return True
-            elif (curMonth >= 1 and curMonth <= endMonth):
-                return True
-        else:
-            if(curMonth >= startMonth and curMonth <= endMonth):
-                return True
-
-        return False
+    if(curMonth in monthRange): return True
+    return False
 
 # O(n) time
 def availCritters(critterType, numCritters):
@@ -66,16 +28,17 @@ def availCritters(critterType, numCritters):
     curMonth = datetime.now().month
     result = list()
 
-    for id in range(1, numCritters, 1):
+    for id in range(1, numCritters + 1, 1):
         critterId = str(id)
         response = requests.get(f'https://acnhapi.com/v1/{critterType}/{critterId}')
-        availableMonths = response.json()['availability']['month-northern']
-        availableTimes = response.json()['availability']['time']
 
-        if(validMonth(availableMonths, curMonth) and validTime(availableTimes, curTime)):
+        month = validMonth(response.json()['availability']['month-array-northern'], curMonth)
+        time = validTime(response.json()['availability']['time-array'], curTime)
+
+        if(month and time):
             result.append(response.json()['file-name']) # this can be changed to support the graphics in index.html
-        
-        return result
+
+    return result
 
 #<------------------Flask------------------>
 
@@ -96,10 +59,10 @@ def index():
     SEANUM = 30
 
     availFish = availCritters("fish", FISHNUM)
-    availBugs = availCritters("bug", BUGNUM)
+    availBugs = availCritters("bugs", BUGNUM)
     availSea = availCritters("sea", SEANUM)
 
-    return render_template('index.html', availFish, availBugs, availSea)
+    return render_template('index.html', availFish = availFish, availBugs = availBugs, availSea = availSea)
 
 if __name__ == '__main__':
     app.run()
